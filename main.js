4d3fc648c8e112c9ee3834f40fb1572f9136b600
Jay <jaewhee@tictag.io>
2022-01-07 19:31:42 +0900

    app

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const dotenv = require("dotenv").config();
+const http = require("http");
+var fs = require('fs');
+var url = require('url');
 
 let app = express();
 let PORT = process.env.PORT;
@@ -10,6 +13,10 @@ let PORT = process.env.PORT;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(compression());
+
+app.get("/", (req, res) => {
+  return res.send("Homepage");
+});
 
 app.listen(PORT, () => {
   console.log("On sail");
